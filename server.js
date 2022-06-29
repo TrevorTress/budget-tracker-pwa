26c3b84559dc0d8c7d8c7ec70cf8107a4fc022f3
TrevorTress <trevor.tress@outlook.com>
2022-06-29 17:05:06 -0400

    updated heroku connection info

--- server.js
+++ server.js
@@ -27,13 +27,3 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-// TO-DO:
-// - TEST /js/idb.js (18.4)
-// - deploy to MongoDB Atlas (18.5)
-
-// GIVEN a budget tracker without an internet connection
-// WHEN the user inputs an expense or deposit
-// THEN they will receive a notification that they have added an expense or deposit
-// WHEN the user reestablishes an internet connection
-// THEN the deposits or expenses added while they were offline are added to their transaction history and their totals are updated
